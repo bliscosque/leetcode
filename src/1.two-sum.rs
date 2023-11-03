f61f48f400dbd328dcdbca6f358228e337009edf
Thiago <bliscosque@yahoo.com.br>
2023-11-02 23:26:55 -0600

    adding within project and module

--- src/1.two-sum.rs
+++ src/1.two-sum.rs
@@ -6,7 +6,7 @@
 
 // @lc code=start
 use std::collections::HashMap;
-impl Solution {
+//impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut hm=HashMap::new();
         let mut ans: Vec<i32>=Vec::new();
@@ -16,12 +16,24 @@ impl Solution {
             if hm.contains_key(&search) {
                 ans.push(i as i32);
                 ans.push(hm[&search]);
+                ans.sort();
                 return ans;
             }
             hm.insert(el,i as i32);
         }
         ans
+
     }
-}
+//}
 // @lc code=end
 
+#[cfg(test)]
+mod tests {
+    use super::two_sum;
+
+    #[test]
+    fn test_two_sum() {
+        // Standard case.
+        assert_eq!(two_sum(vec![1, 2, 3, 4, 5], 9), vec![3, 4]);
+    }
+}
