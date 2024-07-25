5a95fa85b127aa45be536f7fbbc11160454f61a1
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-07-25 17:15:13 -0600

    7 py

--- Python/7.ReverseInteger.py
+++ Python/7.ReverseInteger.py
@@ -1,3 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        ans=0
         
+        positive = x>=0
+        if not positive: x=x*-1
+        
+        while x:
+            dig=x%10
+            ans=ans*10+dig
+            x=x//10
+        
+        if not positive: ans*=-1
+        return ans if -2**31 <= ans <= 2**31-1 else 0
+
+s=Solution()
+print(s.reverse(123))
+print(s.reverse(-123))
+print(s.reverse(120))
