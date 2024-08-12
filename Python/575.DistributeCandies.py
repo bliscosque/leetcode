915726ded9df55d0c9e33d096a89cfce6e3b3998
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-08-12 12:11:57 -0300

    575 py

--- Python/575.DistributeCandies.py
+++ Python/575.DistributeCandies.py
@@ -1,3 +1,11 @@
+from typing import List
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        ...
+        s=set(candyType)
+        n=len(candyType)//2
+        return min(n,len(s))
+
+s=Solution()
+print(s.distributeCandies([1,1,2,2,3,3])) #3
+print(s.distributeCandies([1,1,2,3])) #2
+print(s.distributeCandies([6,6,6,6])) #1
