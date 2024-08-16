deaa3d8ca41ed38a1044c4990c12c888375f84d3
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-08-16 19:05:44 -0300

    323 py v2

--- Python/632.SmallestRangeCoveringElementsfromKLists.py
+++ Python/632.SmallestRangeCoveringElementsfromKLists.py
@@ -1,7 +1,8 @@
 from typing import List
 import math
+import heapq
 class Solution:
-    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+    def smallestRange_list(self, nums: List[List[int]]) -> List[int]:
         n=len(nums)
         cur=[]
         for l in range(n):
@@ -25,6 +26,32 @@ class Solution:
 
         return arange
         
+        #using PQ
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        heap=[(row[0],i,0) for i,row in enumerate(nums)] #(min_val, list_idx, element_index)
+        heapq.heapify(heap)
+        
+        max_val=max(row[0] for row in nums)
+        min_range=float('inf')
+        smallest_range=[]
+
+        while heap:
+            mi,list_idx,el_idx=heapq.heappop(heap)
+            cur_range=max_val-mi
+
+            if cur_range < min_range:
+                min_range=cur_range
+                smallest_range=[mi,max_val]
+            
+            if el_idx+1==len(nums[list_idx]):
+                break
+
+            next_val=nums[list_idx][el_idx+1]
+            max_val=max(max_val,next_val)
+            heapq.heappush(heap,(next_val,list_idx,el_idx+1))
+
+        return smallest_range
+
 s=Solution()
 print(s.smallestRange(nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]])) # [20,24]
 print(s.smallestRange(nums = [[1,2,3],[1,2,3],[1,2,3]])) # [1,1]
