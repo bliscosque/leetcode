e4afded6697d104baca20a6922b3ec4d10505224
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-08-21 17:57:09 -0300

    201 python ok

--- Python/201.BitwiseANDOfNumbersRange.py
+++ Python/201.BitwiseANDOfNumbersRange.py
@@ -2,47 +2,31 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         if left==0: return 0
         if left==right: return left
-        #l and r will be in formar 0000   111  [0|1]
-        # only the "common ones" in the middle should be considered
+        #l and r will be in formar 0000   1[1|0]  [0|1]
+        # only the "middle part" should be considered, this middle part should be the same for l and r
         b_left,b_right=bin(left)[2:].zfill(32),bin(right)[2:].zfill(32)
-        first_l=b_left.index('1')
-        amt_l=0
-        for char in b_left[first_l:]:
-            if char=='1':
-                amt_l+=1
-            else:
-                break
-        print(first_l,amt_l)
-        first_r=b_right.index('1')
-        amt_r=0
-        for char in b_right[first_r:]:
-            if char=='1':
-                amt_r+=1
-            else:
-                break
-        print(first_r,amt_r)
-
+        first_l, first_r=b_left.index('1'), b_right.index('1')
         if first_l!=first_r:
             return 0
         
-        ansst=first_r
-        ansdiff=min(amt_l,amt_r)
-        print(ansst,ansdiff)
+        finaleq=first_r+1
+        while b_left[finaleq]==b_right[finaleq]:
+            finaleq+=1
         
         ans=0
-        for i in range(ansst, ansst+ansdiff):
-            ans+=2**(32-i-1)
+        for i in range(first_l, finaleq):
+            ans+=2**(32-i-1)*int(b_left[i])
         return ans
 
 
         
     
 s=Solution()
-#print(s.rangeBitwiseAnd(5,7)) # 4
-#print(s.rangeBitwiseAnd(3,3)) # 3
-#print(s.rangeBitwiseAnd(6,7)) # 6
+print(s.rangeBitwiseAnd(5,7)) # 4
+print(s.rangeBitwiseAnd(3,3)) # 3
+print(s.rangeBitwiseAnd(6,7)) # 6
 print(s.rangeBitwiseAnd(10,11)) # 10
-#print(s.rangeBitwiseAnd(0,0)) # 0
-#print(s.rangeBitwiseAnd(1,2147483647)) # 0
-#print(s.rangeBitwiseAnd(600000000,2147483645)) # 0
-#print(s.rangeBitwiseAnd(3,6)) # 0
+print(s.rangeBitwiseAnd(0,0)) # 0
+print(s.rangeBitwiseAnd(1,2147483647)) # 0
+print(s.rangeBitwiseAnd(600000000,2147483645)) # 0
+print(s.rangeBitwiseAnd(3,6)) # 0
