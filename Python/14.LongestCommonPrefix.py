44127a52b59169c16856b6b14a282146dc33b67c
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-12-26 12:25:39 -0300

    14 py

---
+++ Python/14.LongestCommonPrefix.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs)==1:
+            return strs[0]
+        
+        minlength=999
+        minw=""
+        for s in strs:
+            if minlength>len(s):
+                minlength=len(s)
+                minw=s
+        
+        if minlength==0:
+            return ""
+        
+        for minpref in range(1,minlength+1):
+            comp=strs[0][:minpref]
+            for s in strs[1:]:
+                if s[:minpref]!=comp:
+                    return comp[:-1]
+
+        return minw
+
+s=Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"])) # fl
+print(s.longestCommonPrefix(["dog","racecar","car"])) # ""
+print(s.longestCommonPrefix([""])) # ""
+print(s.longestCommonPrefix(["",""])) # ""
+print(s.longestCommonPrefix(["ab","a"])) # ""
