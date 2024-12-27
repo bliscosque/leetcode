1b35de915d6baeb42fb6caa199e6749b86e173b8
Thiago Goncalves <bliscosque@yahoo.com.br>
2024-12-27 12:45:05 -0300

    169 py - using Bayer-Moore

--- Python/169.MajorityElement.py
+++ Python/169.MajorityElement.py
@@ -1,12 +1,24 @@
 from typing import List
 import math
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+    def majorityElement_v1(self, nums: List[int]) -> int:
         cnt={}
         maj=(len(nums)+1)//2
         for el in nums:
             cnt[el]=1+cnt.get(el,0)
             if cnt[el]>=maj: return el
+
+    #Bayer-Moore voting system
+    def majorityElement(self, nums: List[int]) -> int:
+        count=ans=0
+        for n in nums:
+            if count==0:
+                ans=n
+            if n==ans:
+                count+=1
+            else:
+                count-=1
+        return ans
 
 s=Solution()        
 print(s.majorityElement( nums = [3,2,3]))
