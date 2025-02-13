c583a93b343465e13ca2fc94684dd71748754380
Thiago Goncalves <bliscosque@yahoo.com.br>
2025-02-13 12:44:55 -0300

    69 py

---
+++ Python/69.Sqrt_x.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r=0,x
+        while l<=r:
+            mid=(l+r)//2
+            if mid*mid<=x<((mid+1)*(mid+1)): return mid
+            elif mid*mid<x:
+                l=mid+1
+            else:
+                r=mid-1
+
+s=Solution()
+print(s.mySqrt(4))
+print(s.mySqrt(8))
